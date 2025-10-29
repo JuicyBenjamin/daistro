3c994d8263c21034f20faa0ed2defab8994ebaeb
Benjamin Vinterberg <benjaminvinterberg@gmail.com>
2025-10-29 16:22:43 +0100

    chore: first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,20 +6,17 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Daistro',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'Components',
 					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
+						{ label: 'Actions', items: [
+							{ label: 'Button', slug: 'components/actions/button' },
+						]}
+					]
+				}
 			],
 		}),
 	],
---
+++ src/content/docs/components/actions/button.md
@@ -0,0 +1,4 @@
+---
+title: 'buttons'
+description: 'yolo'
+---
