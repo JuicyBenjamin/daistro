d49bc7ba7da1497f31b5a9640096d2a67065bf94
Benjamin Vinterberg <benjaminvinterberg@gmail.com>
2025-10-30 16:24:16 +0100

    feat: front page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,45 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Daistro',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Components',
-					items: [
-						{ label: 'Actions', items: [
-							{ label: 'Button', slug: 'components/actions/button' },
-						]}
-					]
-				}
-			],
-		}),
-	],
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': '/src',
+      },
+    },
+  },
+  integrations: [
+    starlight({
+      title: 'Daistro',
+      social: [
+        {
+          icon: 'github',
+          label: 'GitHub',
+          href: 'https://github.com/withastro/starlight',
+        },
+      ],
+      favicon: '/favicon.png',
+      customCss: ['/globals.css'],
+      sidebar: [
+        {
+          label: 'Getting Started',
+          items: [{ label: 'Getting Started', slug: 'getting-started' }],
+        },
+        {
+          label: 'Components',
+          items: [
+            {
+              label: 'Actions',
+              items: [{ label: 'Button', slug: 'components/actions/button' }],
+            },
+          ],
+        },
+      ],
+    }),
+  ],
 });
